9746b7359c21b25e97cfb84606da9c7c602d615a
frenchfries11234 <45515297+frenchfries11234@users.noreply.github.com>
2018-12-01 14:46:30 -0500

    Add files via upload

--- backend/stocksoneel.py
+++ backend/stocksoneel.py
@@ -12,46 +12,47 @@ import matplotlib as plt
 import requests
 import datetime
 def getStock(name,itrval):
-    # '1min', '5min', '15min', '30min', '60min'
+    '''
+    returns a list of most recent 100 values for the given stock (abbriviated) and interval 
+    this takes the average of the high and low values
+    intervals options are: 'minute','day','week','month'
+    '''
     ts = TimeSeries(key='C5VMXSQYDFRMISJ2', output_format='pandas')
-    data, meta_data = ts.get_intraday(symbol=name,interval=str(itrval)+'min', outputsize='full')
-    avg=[]
+    if itrval=='minute':data, meta_data = ts.get_intraday(symbol=name,interval='1min', outputsize='full')
+    elif itrval=='day':data=ts.get_daily(name,100)[0]
+    elif itrval=='week':data=ts.get_weekly(name,100)[0]
+    elif itrval=='month':data=ts.get_monthly(name,100)[0]
     for i in range(len(data['2. high'])):
         avg.append((data['2. high'][i]+data['3. low'])/2)
     return avg
 
-def price(symbol, comparison_symbols=['USD'], exchange=''):
+def BCprice(symbol, comparison_symbols=['USD'], exchange=''):
     url = 'https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}'\
             .format(symbol.upper(), ','.join(comparison_symbols).upper())
     if exchange:
         url += '&e={}'.format(exchange)
     page = requests.get(url)
     data = page.json()
-    return data
-def daily_price_historical(symbol, comparison_symbol, all_data=True, limit=100, aggregate=1, exchange=''):
-    url = 'https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}&limit={}&aggregate={}'\
-            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate)
-    if exchange:
-        url += '&e={}'.format(exchange)
-    if all_data:
-        url += '&allData=true'
+     return data
+
+def getCrypto(symbol,itrval):
+    '''
+    returns a list of most recent 100 values for the given cryptocurrency(abbrieviated) and interval
+    this takes the average of the high and low values
+    intervals options are: 'minute','hour','day','week','month'
+    '''
+    url = 'https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit={}&aggregate={}'.format(symbol.upper(),'USD', 100,1)
     page = requests.get(url)
     data = page.json()['Data']
     df = pd.DataFrame(data)
     df['timestamp'] = [datetime.datetime.fromtimestamp(d) for d in df.time]
-    return df
+    avg=[]
+    for i in range(len(df['high'])):
+        avg.append((df['high'][i]+df['low'][i])/2)
+    return avg
 
-def hourly_price_historical(symbol, comparison_symbol, limit=100, aggregate=1, exchange=''):
-    url = 'https://min-api.cryptocompare.com/data/histohour?fsym={}&tsym={}&limit={}&aggregate={}'\
-            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate)
-    if exchange:
-        url += '&e={}'.format(exchange)
-    page = requests.get(url)
-    data = page.json()['Data']
-    df = pd.DataFrame(data)
-    df['timestamp'] = [datetime.datetime.fromtimestamp(d) for d in df.time]
-    return df
 
-print(getStock('GOOGL',1))
-print(daily_price_historical("BTC","USD").to_string())
-print(hourly_price_historical("BTC","USD").to_string())
+print(getCrypto('BTC','day'))
+#print(getStock('GOOGL',1))
+#print(daily_price_historical("BTC","USD").to_string())
+#print(hourly_price_historical("BTC","USD").to_string())
