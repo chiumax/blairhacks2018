8d75dccf6556d038d44d3bce84e27503fa9ecfb7
Christopher Stallard <chrisstallard4@gmail.com>
2018-12-01 15:34:54 -0500

    fix flask even more (maybe)

--- backend/serve.py
+++ backend/serve.py
@@ -3,14 +3,12 @@ from flask import request
 from passlib.hash import pbkdf2_sha512
 from pymongo import MongoClient
 from flask_cors import CORS, cross_origin
+from flask_pymongo import PyMongo
 
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
-
-
-c = MongoClient('localhost',27017)
-db = c.test
-user_info = db.user_info
-current_users = db.loggedin_users
 
 app = Flask(__name__)
 CORS(app)
@@ -26,7 +24,7 @@ def register():
         print(uname, pw)
         h=pbkdf2_sha512.hash(pw.encode())
         entry={"uname":uname, "hashed_pw":h, "money": 10000, "stocks":{}}
-        user_info.insert_one(entry)
+        mongo.db.mongo.db.user_info.insert_one(entry)
         return 'success'
 
 @app.route('/login')
@@ -34,14 +32,14 @@ def login():
     if requests.method=='POST':
         j=request.json()
         print(j)
-        uname=request.args.get('uname')
-        pw=request.args.get('pw')
-        user = user_info.find_one({"uname": uname})
+        uname=j['uname']
+        pw=j['pw']
+        user = mongo.db.user_info.find_one({"uname": uname})
         if user:
             if pbkdf2_sha512.verify(pw,user["hash"]):
                 response="success"
                 user_id=uuid.uuid4().hex()
-                current_users.insert_one({"id":user_id,"uname":uname})
+                mongo.db.current_users.insert_one({"id":user_id,"uname":uname})
                 response.set_cookie("SessionID", userid)
                 return response
     return "fail"
@@ -49,13 +47,15 @@ def login():
 @app.route('/buy')
 def buy():
     if request.method=='POST':
+        j=request.json()
+        print(j)
         user_id=request.cookies.get("SessionID")
-        user=current_users.find_one({"id":user_id})
-        buy_order=request.args.get('money')
+        user=mongo.db.current_users.find_one({"id":user_id})
+        buy_order=j['money']
         current_money=user["money"]
         if current_money<buy_order:
             return 'not enough money'
-        stock_name=request.args.get("StockName")
+        stock_name=j["StockName"]
         stock_price = 1000 #TODO: Stock Price Function
         num_stocks=current_money/stock_price
         if not user["stocks"][stock_name]:
@@ -68,11 +68,13 @@ def buy():
 @app.route("/sell")
 def sell():
     if request.method=='POST':
+        j=request.json()
+        print(j)
         user_id=request.cookies.get("SessionID")
-        user=current_users.find_one({"id":user_id})
-        sell_order=request.args.get('num_stocks')
+        user=mongo.db.current_users.find_one({"id":user_id})
+        sell_order=j['num_stocks']
         current_money=user["money"]
-        stock_name=request.args.get("StockName")
+        stock_name=j["StockName"]
         stock_price = 1000 #TODO: Stock Price Function
         if user["stocks"][stock_name]>=sell_order:
             user["money"]=current_money+sell_order*stock_price
@@ -83,12 +85,12 @@ def sell():
 def getMoney():
     if request.method=='POST':
         user_id=request.cookies.get("SessionID")
-        user=current_users.find_one({"id":user_id})
+        user=mongo.db.current_users.find_one({"id":user_id})
         return user["money"]
-        
+
 @app.route("/getStocks")
 def getStocks():
     if request.method=='POST':
         user_id=request.cookies.get("SessionID")
-        user=current_users.find_one({"id":user_id})
+        user=mongo.db.current_users.find_one({"id":user_id})
         return user["stocks"]
